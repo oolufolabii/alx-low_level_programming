674157282c9d4df550d91e72aabc3426bba6d85b
oolufolabii <otitoola.olufolabi@gmail.com>
2022-04-07 12:52:35 -0700

    C Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-  writ(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
        STRLEN("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 
   return (1);
