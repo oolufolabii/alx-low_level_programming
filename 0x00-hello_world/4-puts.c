aa126270378f2e03b1e76ce372d0cd6c43f5b59a
oolufolabii <otitoola.olufolabi@gmail.com>
2022-04-07 08:55:49 -0700

    Hello World C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
+/**
+ *The function uses put, 
+ *Printf function is not used
+ *
+ */
 int main(void)
 {
-
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
-  
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+/**
+ *Function using printf instead of puts
+ *
+ */
 int main(void)
 {
-
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-
-  return(0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
