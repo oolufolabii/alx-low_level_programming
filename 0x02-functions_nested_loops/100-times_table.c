29581c942b8990ac123c1d36e870de0c06502642
oolufolabii <otitoola.olufolabi@gmail.com>
2022-04-12 15:39:20 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
 void print_times_table(int n)
 {
 int i, j, k;
-if (n >= 0 && n <= 15) //n cannot go beyond 15
+if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
@@ -40,7 +40,7 @@ _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
-} //units tens hundreds
+} 
 }
 _putchar('\n');
 }
