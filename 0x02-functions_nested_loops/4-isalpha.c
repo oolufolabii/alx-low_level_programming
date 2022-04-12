51c9e63dfe0318c6eb9e8a3728ebe051365b9400
oolufolabii <otitoola.olufolabi@gmail.com>
2022-04-12 11:07:03 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
+ * _isalpha - checks for lowercase character
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
