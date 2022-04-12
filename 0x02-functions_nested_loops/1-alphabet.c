d15d9eb546259cbd2d3c20fb52052a9559472c94
oolufolabii <otitoola.olufolabi@gmail.com>
2022-04-12 10:07:38 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Void.
+*/
+
+void print_alphabet(void)
+{
+int i = 97;
+while (i <= 122)
+{
+_putchar(i);
+_putchar('\n');
+i++;
+}
+}
