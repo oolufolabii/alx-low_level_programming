cf7a3a8059fab6f016684d9629a3e20caa0d72ed
Olufolabi <101478509+oolufolabii@users.noreply.github.com>
2022-04-14 14:11:36 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,15 +4,14 @@
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
-
 void print_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 48; i < 58; i++)
-{
-_putchar(i);
-}
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
 
-_putchar(10);
+	_putchar('\n');
 }
