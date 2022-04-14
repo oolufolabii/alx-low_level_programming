a19ad028ee9ac6d94dd4256226931572695708f3
Olufolabi <101478509+oolufolabii@users.noreply.github.com>
2022-04-14 14:10:04 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@
  */
 void more_numbers(void)
 {
-int num, i;
-for (i = 0; i < 10; i++)
-{
+	int num, i;
 
-for (num = 0; num <= 14; num++)
-{
-if (num > 9)
-_putchar((num / 10) + '0');
-_putchar((num % 10) + '0');
-}
-_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
