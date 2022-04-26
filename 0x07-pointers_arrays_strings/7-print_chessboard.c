87267c92ef0f28f6ead69d0b5bc48a720f36b616
oolufolabii <otitoola.olufolabi@gmail.com>
2022-04-26 08:40:30 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,22 +2,21 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: 2D array to print
+ * @a: input pointer.
+ * Return: null return.
  */
-
 void print_chessboard(char (*a)[8])
 {
-int i, j;
+unsigned int i, j = 0;
 
-for (i = 0; i < 8; i++)
+for (i = 0; i < 64; i++)
 {
-
-for (j = 0; j < 8; j++)
+if (i % 8 == 0 && i != 0)
 {
-
-_putchar(a[i][j]);
-}
-
+j = i;
 _putchar('\n');
 }
+_putchar(a[i / 8][i - j]);
+}
+_putchar('\n');
 }
