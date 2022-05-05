32e82118432a7bb3a1ce0e690501a43955117f5a
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-05 13:52:24 +0100

    0x0B. C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,3 @@
-#include "main.h"
-
-
 /**
  * *_strdup - copies the string given as parameter
  * @str: string to duplicate
@@ -26,9 +23,8 @@ copier = malloc(sizeof(char) * (len + 1));
 if (copier == NULL)
 return (NULL);
 
-
-for (a = 0; a <= ((sizeof(str)) + 1); a++)
-copier[a] = str[a];
+while ((copier[a] = str[a]) != '\0')
+a++;
 
 return (copier);
 }
