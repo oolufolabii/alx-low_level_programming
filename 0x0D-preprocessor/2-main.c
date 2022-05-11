1f323a143a422d3d4e00652c2dc0a346cf6c47f8
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-11 12:00:50 +0100

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - C file to prints the name of the file
+ * Return: 0 for successful completion
+ */
+int main(void)
+{
+
+printf("%s\n", __FILE__);
+
+return (0);
+}
