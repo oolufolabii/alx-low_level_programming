0776c3da5292e0030e8327f6d7ca9e4920d778d3
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-24 14:09:30 +0100

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,13 @@
-global _start
+	global	main
+	section	.text
+main:	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, message
+	mov	rdx, 17
+	syscall
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
 
-section .text
-
-_start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
-  syscall           ; );
-
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, Holberton", 10
+	section	.data
+message:	db	"Hello, Holberton", 0xa
