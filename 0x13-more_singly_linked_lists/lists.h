d5e0030540361f0bbea543101bd261a4cde1e90e
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-26 13:39:50 +0100

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
@@ -18,7 +17,6 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
