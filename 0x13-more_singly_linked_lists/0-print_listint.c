376c1d297864ff9161415af8b6ba50ffd5343c7c
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-26 11:19:27 +0100

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+
+/**
+ * print_listint - A string list funtion to print content of a list.
+ * @h: Argument variable of linked list type to print.
+ * Return: Amount of nodes counted
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
