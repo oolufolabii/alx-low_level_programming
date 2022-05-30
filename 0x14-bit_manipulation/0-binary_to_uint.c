cfb14cdefdf1276db831de48c09ae093a242e85c
oolufolabii <otitoola.olufolabi@gmail.com>
2022-05-30 18:16:18 +0100

    Bit Manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function that converts Binary to Decimals.
+ * @b: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decimal = 2 * decimal + (b[i] - '0');
+	}
+
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - Function that converts Decimals to Binary.
+ * @n: argument of function.
+ * Return: Always 0 (Success)
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n < 2)
+		_putchar(n + '0');
+	else
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
