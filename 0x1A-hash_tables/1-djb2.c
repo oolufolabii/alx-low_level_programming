e15db1f1cf72ee422dde1e675ec51b8661dce442
oolufolabii <otitoola.olufolabi@gmail.com>
2022-07-29 12:10:36 +0100

    C - Hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,13 +6,13 @@
  *
  * Return: hash value
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash = 5381;
+	unsigned long int hash;
 	int c;
 
-	while (c = *str++)
+	hash = 5381;
+	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
