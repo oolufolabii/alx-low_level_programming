34c24c4f728435499e8cbc6f194e224eae3c9d40
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-01 13:24:57 +0100

    Search Algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,1 +1,14 @@
 #include "search_algos.h"
+
+/**
+ * jump_search - Search for a value in an array using interpolation search
+ *
+ * @array: Pointer to the first element of the array to search
+ * @size: The length of the array
+ * @value: The value to search for
+ * Return: Return the value at the first index otherwise -1 if not present
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+}
